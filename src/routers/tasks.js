7a2eaa3da82cc0b309a4c9b3a2126314df910ad5
Jayavardhan <jayaedde@gmail.com>
2022-11-12 21:50:39 +0530

    GET tasksByID

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -66,3 +66,16 @@ router.get("/tasks", auth, async (req, res) => {
     res.status(500).send();
   }
 });
+
+router.get("/tasks/:id", auth, async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findOne({ _id, owner: req.user._id });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(200).send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+});

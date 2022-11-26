fa4a5064e38e3878cfbdf97026bd208e4b047337
Jayavardhan <jayaedde@gmail.com>
2022-11-26 22:12:43 +0530

    refacotred the main app for testing

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-const express = require("express");
-require("./db/mongoose");
-const userRouter = require("./routers/users");
-const taskRouter = require("./routers/tasks");
-
-const path = require("path");
-const app = express();
-
-const port = process.env.PORT || 8000;
-const publicDirectoryPath = path.join(__dirname, "../public");
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(publicDirectoryPath));
-app.use(express.json());
-//Mount routers
-app.use(userRouter);
-app.use(taskRouter);
+const app = require("./app");
+const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log("server is up", port);
+  console.log("Server is on port" + port);
 });
